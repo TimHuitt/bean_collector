845267849db20908a9c6f5b9294ac85b0ce45ee5
Tim Huitt <timhuitt@gmail.com>
2024-01-10 09:43:37 -0700

    create model, add items

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Bean(models.Model):
+  name = models.CharField(max_length=30)
+  origin = models.CharField(max_length=40)
+  description = models.TextField(max_length=250)
+  cooking_time = models.CharField(max_length=50)
+  image = models.TextField(max_length=250)
+  common_uses = models.TextField(max_length=250)
+  nutritional_info = models.TextField(max_length=250)
